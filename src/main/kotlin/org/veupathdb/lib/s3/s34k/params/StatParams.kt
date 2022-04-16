4e00aceaf3730501997e0400d69e8455fae3ef5f
Elizabeth <foxcapade@gmail.com>
2022-04-16 17:11:20 -0400

    stat params conversion

--- src/main/kotlin/org/veupathdb/lib/s3/s34k/params/StatParams.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/params/StatParams.kt
@@ -1,4 +1,27 @@
 package org.veupathdb.lib.s3.s34k.params
 
-class StatParams {
+import org.slf4j.LoggerFactory
+import org.veupathdb.lib.s3.s34k.S3ObjectMeta
+import org.veupathdb.lib.s3.s34k.params.`object`.ObjectStatParams
+
+/**
+ * Generic Stat Operation Parameters
+ *
+ * @constructor Constructs a new [StatParams] instance.
+ *
+ * @param callback Optional callback that will be called upon request
+ * completion.
+ *
+ * @author Elizabeth Paige Harper [https://github.com/Foxcapades]
+ *
+ * @since v0.1.0
+ */
+class StatParams(var callback: ((S3ObjectMeta) -> Unit)? = null) : AbstractRequestParams() {
+
+  private val Log = LoggerFactory.getLogger(this::class.java)
+
+  fun toObjectStatParams(path: String): ObjectStatParams {
+    Log.trace("toObjectStatParams(path = {})", path)
+    return ObjectStatParams(path, callback)
+  }
 }
