0b7bca0b2624edc38313615ad432ab5906663224
Elizabeth <foxcapade@gmail.com>
2022-04-16 17:00:32 -0400

    conversion methods

--- src/main/kotlin/org/veupathdb/lib/s3/s34k/params/ExistsParams.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/params/ExistsParams.kt
@@ -1,5 +1,9 @@
 package org.veupathdb.lib.s3.s34k.params
 
+import org.slf4j.LoggerFactory
+import org.veupathdb.lib.s3.s34k.params.bucket.BucketExistsParams
+import org.veupathdb.lib.s3.s34k.params.`object`.ObjectExistsParams
+
 /**
  * Exists Check Operation Parameters
  *
@@ -13,6 +17,26 @@ package org.veupathdb.lib.s3.s34k.params
  * @since v0.1.0
  */
 class ExistsParams(var callback: ((Boolean) -> Unit)? = null) : AbstractRequestParams() {
+
+  private val Log = LoggerFactory.getLogger(this::class.java)
+
+  // TODO: Document me
+  fun toObjectExistsParams(path: String): ObjectExistsParams {
+    Log.trace("toObjectExistsParams(path = {})", path)
+    return ObjectExistsParams(path, callback).also {
+      (it.headers as MutableMap).putAll(headers)
+      (it.queryParams as MutableMap).putAll(queryParams)
+    }
+  }
+
+  fun toBucketExistsParams(bucket: String): BucketExistsParams {
+    Log.trace("toBucketExistsParams(bucket = {})", bucket)
+    return BucketExistsParams(bucket, callback).also {
+      (it.headers as MutableMap).putAll(headers)
+      (it.queryParams as MutableMap).putAll(queryParams)
+    }
+  }
+
   override fun toString(): String {
     val out = StringBuilder(2048)
 
