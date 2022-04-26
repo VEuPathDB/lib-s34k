cf446a643a7375025f27c21716374fa13ea272f3
Elizabeth <foxcapade@gmail.com>
2022-04-26 09:44:30 -0400

    add missing type fields

--- src/main/kotlin/org/veupathdb/lib/s3/s34k/requests/object/S3ObjectTouchParams.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/requests/object/S3ObjectTouchParams.kt
@@ -1,3 +1,13 @@
 package org.veupathdb.lib.s3.s34k.requests.`object`
 
-interface S3ObjectTouchParams : S3ObjectWriteParams
+import org.veupathdb.lib.s3.s34k.response.`object`.S3Object
+
+// TODO: Document me
+interface S3ObjectTouchParams : S3ObjectWriteParams {
+
+  // TODO: Document me
+  var contentType: String?
+
+  // TODO: Document me
+  var callback: ((S3Object) -> Unit)?
+}
