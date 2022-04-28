27d61ca243caf20374e0386039452b3bb8ef87a8
Elizabeth <foxcapade@gmail.com>
2022-04-28 08:16:45 -0400

    docs

--- src/main/kotlin/org/veupathdb/lib/s3/s34k/requests/bucket/S3BucketWriteRequestParams.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/requests/bucket/S3BucketWriteRequestParams.kt
@@ -2,9 +2,23 @@ package org.veupathdb.lib.s3.s34k.requests.bucket
 
 import org.veupathdb.lib.s3.s34k.fields.tags.S3MutableTagMap
 
-// TODO: Document me
+/**
+ * S3 Bucket Write Request
+ *
+ * Base type for S3 operations that create or otherwise write to a bucket.
+ *
+ * This type includes optional tags that may be attached to the bucket during
+ * the operation.
+ *
+ * @author Elizabeth Paige Harper [https://github.com/Foxcapades]
+ *
+ * @since v0.1.0
+ */
 interface S3BucketWriteRequestParams : S3BucketRequestParams {
 
-  // TODO: Document me
+  /**
+   * Tags that may be attached to the target bucket during the execution of
+   * this S3 operation.
+   */
   val tags: S3MutableTagMap
 }
--- src/main/kotlin/org/veupathdb/lib/s3/s34k/requests/object/ObjectDeleteError.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/requests/object/ObjectDeleteError.kt
@@ -2,11 +2,34 @@ package org.veupathdb.lib.s3.s34k.requests.`object`
 
 import org.veupathdb.lib.s3.s34k.errors.S34kException
 
+/**
+ * Object deletion error.
+ *
+ * Represents any error that occurred as a result of attempting to delete a
+ * single object from an S3 store.
+ *
+ * Bulk object deletes may include more than one instance of this exception as
+ * details about the objects that could not be deleted.
+ *
+ * @author Elizabeth Paige Harper [https://github.com/Foxcapades]
+ *
+ * @since v0.1.0
+ */
 class ObjectDeleteError : S34kException {
+
+  /**
+   * Key/path of the object on which the delete operation was attempted.
+   */
   val path: String
 
+  /**
+   * Message returned by the S3 server.
+   */
   val s3Message: String
 
+  /**
+   * Error code returned by the S3 server.
+   */
   val s3Code: String
 
   constructor(
