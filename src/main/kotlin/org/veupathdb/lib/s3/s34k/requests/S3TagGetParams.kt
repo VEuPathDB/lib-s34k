465fe944d9a372706aa6f4fbb75ebcf6ce9f643d
Elizabeth <foxcapade@gmail.com>
2022-05-02 12:49:33 -0400

    add tag set field

--- src/main/kotlin/org/veupathdb/lib/s3/s34k/requests/S3TagGetParams.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/requests/S3TagGetParams.kt
@@ -1,8 +1,15 @@
 package org.veupathdb.lib.s3.s34k.requests
 
+import org.veupathdb.lib.s3.s34k.fields.tags.S3MutableTagSet
 import org.veupathdb.lib.s3.s34k.fields.tags.S3TagMap
 
 interface S3TagGetParams : S3RegionRequestParams {
+
+  /**
+   * Optionally filter the returned tags to only those that appear in this tag
+   * set.  If this tag set is empty, all tags will be returned.
+   */
+  val tags: S3MutableTagSet
 
   /**
    * Callback that will be executed on successful completion of the S3
