9d153fda1562cb713a4875b3d433896c8d5cc0da
Elizabeth <foxcapade@gmail.com>
2022-05-04 12:46:44 -0400

    add callbac

--- src/main/kotlin/org/veupathdb/lib/s3/s34k/params/S3TagCreateParams.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/params/S3TagCreateParams.kt
@@ -1,6 +1,7 @@
 package org.veupathdb.lib.s3.s34k.params
 
 import org.veupathdb.lib.s3.s34k.fields.S3MutableTagMap
+import org.veupathdb.lib.s3.s34k.fields.S3TagMap
 
 /**
  * Base interface for S3 operation configurations that can send tags to the S3
@@ -16,4 +17,13 @@ interface S3TagCreateParams : S3RegionRequestParams {
    * Tags that will be attached to the target object.
    */
   val tags: S3MutableTagMap
+
+  /**
+   * Optional callback that will be executed on successful completion of the S3
+   * operation.
+   *
+   * This callback will be passed an [S3TagMap] instance containing all the tags
+   * that were attached to the S3 target.
+   */
+  var callback: ((tags: S3TagMap) -> Unit)?
 }
