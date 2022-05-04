762e2ccdcb57d0a3dcb3184255f0fd7cdce54c32
Elizabeth <foxcapade@gmail.com>
2022-05-04 18:42:27 -0400

    target delete phase

---
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/params/tag/TargetedTagDeleteError.kt
@@ -0,0 +1,57 @@
+package org.veupathdb.lib.s3.s34k.params.tag
+
+import org.veupathdb.lib.s3.s34k.S3Object
+import org.veupathdb.lib.s3.s34k.errors.S34KError
+
+open class TargetedTagDeleteError : S34KError {
+
+  val target: S3Object
+
+  val params: S3TagDeleteParams
+
+  val phase: TargetedTagDeletePhase
+
+  constructor(
+    target: S3Object,
+    phase:  TargetedTagDeletePhase,
+    params: S3TagDeleteParams
+  ) : super("Tag deletion failed in phase $phase") {
+    this.target = target
+    this.params = params
+    this.phase  = phase
+  }
+
+  constructor(
+    target:  S3Object,
+    phase:   TargetedTagDeletePhase,
+    params:  S3TagDeleteParams,
+    message: String
+  ) : super(message) {
+    this.target = target
+    this.params = params
+    this.phase  = phase
+  }
+
+  constructor(
+    target: S3Object,
+    phase:  TargetedTagDeletePhase,
+    params: S3TagDeleteParams,
+    cause:  Throwable
+  ) : super("Tag deletion failed in phase $phase", cause) {
+    this.target = target
+    this.params = params
+    this.phase  = phase
+  }
+
+  constructor(
+    target:  S3Object,
+    phase:   TargetedTagDeletePhase,
+    params:  S3TagDeleteParams,
+    message: String,
+    cause:   Throwable,
+  ) : super(message, cause) {
+    this.target = target
+    this.params = params
+    this.phase  = phase
+  }
+}
---
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/params/tag/TargetedTagDeletePhase.kt
@@ -0,0 +1,7 @@
+package org.veupathdb.lib.s3.s34k.params.tag
+
+enum class TargetedTagDeletePhase {
+  Get,
+  Delete,
+  Put
+}
