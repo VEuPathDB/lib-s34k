12a2c2b2bf9b18beef24aae067ecd6657ddb5550
Elizabeth <foxcapade@gmail.com>
2022-05-05 14:35:05 -0400

    correct callback refs

--- src/main/kotlin/org/veupathdb/lib/s3/s34k/params/bucket/put/BPGetParams.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/params/bucket/put/BPGetParams.kt
@@ -3,5 +3,5 @@ package org.veupathdb.lib.s3.s34k.params.bucket.put
 import org.veupathdb.lib.s3.s34k.params.S3RequestParams
 
 interface BPGetParams : S3RequestParams {
-  var callback: (() -> Unit)
+  var callback: (() -> Unit)?
 }
