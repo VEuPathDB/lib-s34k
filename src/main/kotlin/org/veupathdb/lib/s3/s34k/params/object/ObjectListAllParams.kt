8c832800a31cd80aaf9ce6a633c0ae9b5ef10a3a
Elizabeth <foxcapade@gmail.com>
2022-05-08 17:38:50 -0400

    updates

--- src/main/kotlin/org/veupathdb/lib/s3/s34k/params/object/ObjectListAllParams.kt
+++ src/main/kotlin/org/veupathdb/lib/s3/s34k/params/object/ObjectListAllParams.kt
@@ -3,7 +3,7 @@ package org.veupathdb.lib.s3.s34k.params.`object`
 import org.veupathdb.lib.s3.s34k.objects.ObjectList
 import org.veupathdb.lib.s3.s34k.params.RegionRequestParams
 
-interface ObjectListParams : RegionRequestParams {
+interface ObjectListAllParams : RegionRequestParams {
 
   /**
    * Callback that will be executed on successful completion of the S3
